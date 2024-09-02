93ed973b0d2022e37b0e0a1718c15533e054d397
Andriiko <lesiv433@gmail.com>
2024-09-02 21:18:38 +0300

    basic frontend groundwork, installing bootstrap

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from '../core/components/register/register.component';
+import { LoginComponent } from '../core/components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: "register", component: RegisterComponent },
+    { path: "login", component: LoginComponent },
+];
---
+++ client/src/modules/profile/pages/continue-watching/continue-watching.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-continue-watching',
+  standalone: true,
+  imports: [],
+  templateUrl: './continue-watching.component.html',
+  styleUrl: './continue-watching.component.scss'
+})
+export class ContinueWatchingComponent {
+
+}
