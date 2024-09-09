2564a4f7d0dcfaa34eb071158470156143d36b95
Andriiko <lesiv433@gmail.com>
2024-09-09 21:43:21 +0300

    a bit modal styling

--- client/src/core/components/login/login.component.ts
+++ client/src/core/components/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { BaseModalWindowComponent } from '../../../modules/common-shared/services/modal-dialog.service';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent extends BaseModalWindowComponent {
+  xMark = faXmark;
+
   constructor(protected override readonly bsModalRef: BsModalRef) {
     super(bsModalRef);
   }
