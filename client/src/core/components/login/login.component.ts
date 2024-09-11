1331a346fb302fe41b954ef0c8e7f123b682af0c
Andriiko <lesiv433@gmail.com>
2024-09-11 17:40:31 +0300

    remove fa icon

--- client/src/core/components/login/login.component.ts
+++ client/src/core/components/login/login.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
 import { BaseModalWindowComponent } from '../../../modules/common-shared/services/modal-dialog.service';
 import { BsModalRef } from 'ngx-bootstrap/modal';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent extends BaseModalWindowComponent {
-  xMark = faXmark;
 
   constructor(protected override readonly bsModalRef: BsModalRef) {
     super(bsModalRef);
