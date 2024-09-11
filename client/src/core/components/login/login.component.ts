f8d9405f8e2911b6bd467e7aa862ffe7964b5731
Andriiko <lesiv433@gmail.com>
2024-09-11 19:45:49 +0300

    more login styling

--- client/src/core/components/login/login.component.ts
+++ client/src/core/components/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { BaseModalWindowComponent } from '../../../modules/common-shared/services/modal-dialog.service';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent extends BaseModalWindowComponent {
+
+  username = new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]);
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
 
   constructor(protected override readonly bsModalRef: BsModalRef) {
     super(bsModalRef);
@@ -17,5 +21,9 @@ export class LoginComponent extends BaseModalWindowComponent {
 
   close() {
     this.closeDialog();
+  }
+
+  login() {
+    console.log('logged in')
   }
 }
