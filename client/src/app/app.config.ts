eaa0ecaff1a758697f258809ad4ed220546c33bf
Andriiko <lesiv433@gmail.com>
2024-09-15 13:26:26 +0300

    use functional interceptor

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,22 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
-import { AuthInterceptor } from '../core/services/auth.interceptor';
+import { provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { authInterceptor } from '../core/services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(
+      withInterceptors([authInterceptor]),
       withFetch(),
       withInterceptorsFromDi()
     ),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    },
     provideClientHydration(),
   ]
 };
--- client/src/core/services/auth.interceptor.ts
+++ client/src/core/services/auth.interceptor.ts
@@ -1,16 +1,17 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpHandlerFn, HttpRequest } from "@angular/common/http";
+import { inject } from "@angular/core";
 import { AuthService } from "./auth.service";
-import { from, Observable, switchMap } from "rxjs";
+import { from, switchMap } from "rxjs";
 
-// TODO: Consider using functional interceptor https://angular.dev/guide/http/interceptors
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  constructor(private readonly authService: AuthService) {}
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return from(this.authService.getAuthorizationHeaders()).pipe(
-      switchMap(headers => next.handle(req.clone({ setHeaders: headers })))
-    );
-  }
+export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
+  const authService = inject(AuthService);
+  
+  return from(authService.getAuthorizationHeaders()).pipe(
+    switchMap(headers => {
+      const newReq = req.clone({
+        setHeaders: headers,
+      });
+      return next(newReq);
+    })
+  );
 }
