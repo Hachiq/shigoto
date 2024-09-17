b8e12a4b47c4b3778f6c4e8643d942f4a005ff55
Andriiko <lesiv433@gmail.com>
2024-09-17 21:16:33 +0300

    user profile component structure

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from '../core/components/register/register.component';
 import { LoginComponent } from '../core/components/login/login.component';
+import { HomeComponent } from '../modules/home/home.component';
+import { ProfileComponent } from '../modules/user/pages/profile/profile.component';
+import { ContinueWatchingComponent } from '../modules/user/pages/continue-watching/continue-watching.component';
+import { WatchListComponent } from '../modules/user/pages/watch-list/watch-list.component';
+import { SettingsComponent } from '../modules/user/pages/settings/settings.component';
+import { NotificationComponent } from '../modules/user/pages/notification/notification.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
     { path: "register", component: RegisterComponent },
     { path: "login", component: LoginComponent },
+    { path: "user/profile", component: ProfileComponent },
+    { path: "user/continue-watching", component: ContinueWatchingComponent },
+    { path: "user/watch-list", component: WatchListComponent },
+    { path: "user/notification", component: NotificationComponent },
+    { path: "user/settings", component: SettingsComponent },
 ];
