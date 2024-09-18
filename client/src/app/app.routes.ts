5c97edac35f4b554948af7ecaebb70e27ca3f3de
Andriiko <lesiv433@gmail.com>
2024-09-18 19:59:14 +0300

    update routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,15 +7,23 @@ import { ContinueWatchingComponent } from '../modules/user/pages/continue-watchi
 import { WatchListComponent } from '../modules/user/pages/watch-list/watch-list.component';
 import { SettingsComponent } from '../modules/user/pages/settings/settings.component';
 import { NotificationComponent } from '../modules/user/pages/notification/notification.component';
+import { UserComponent } from '../modules/user/user.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: "register", component: RegisterComponent },
-    { path: "login", component: LoginComponent },
-    { path: "user/profile", component: ProfileComponent },
-    { path: "user/continue-watching", component: ContinueWatchingComponent },
-    { path: "user/watch-list", component: WatchListComponent },
-    { path: "user/notification", component: NotificationComponent },
-    { path: "user/settings", component: SettingsComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    {
+        path: 'user',
+        component: UserComponent,
+        children: [
+          { path: 'profile', component: ProfileComponent },
+          { path: 'continue-watching', component: ContinueWatchingComponent },
+          { path: 'watch-list', component: WatchListComponent },
+          { path: 'notification', component: NotificationComponent },
+          { path: 'settings', component: SettingsComponent },
+          { path: '', redirectTo: 'profile', pathMatch: 'full' },
+        ]
+      },
 ];
