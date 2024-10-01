2198aa2aa61ee2d54fd7468fcda9a85149548035
Andriiko <lesiv433@gmail.com>
2024-10-01 17:17:39 +0300

    replace *ngIf with @if()

--- client/src/modules/user/user.component.ts
+++ client/src/modules/user/user.component.ts
@@ -4,12 +4,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faUser, faHistory, faHeart, faBell, faCog, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { AuthService } from '../../core/services/auth.service';
 import { User } from '../common-shared/models/user';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [RouterModule, FontAwesomeModule, CommonModule],
+  imports: [RouterModule, FontAwesomeModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
@@ -21,14 +20,12 @@ export class UserComponent implements OnInit {
   icog = faCog;
   iarrowRight = faArrowRight;
 
-  isAuthenticated: boolean | undefined;
   user?: User;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.authService.isAuthenticated$.subscribe(async auth => {
-      this.isAuthenticated = auth;
       this.user = await this.authService.getCurrentUser();
     });
   }
