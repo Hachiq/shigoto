b877afd6a3462d480626dc783a87c11d1a42c319
Andriiko <lesiv433@gmail.com>
2024-10-01 15:16:28 +0300

    show the description for the first 4 items

--- client/src/modules/main-list/components/anime-list-item/anime-list-item.component.ts
+++ client/src/modules/main-list/components/anime-list-item/anime-list-item.component.ts
@@ -1,17 +1,24 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { TextBuilderService } from '../../services/text-builder.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-anime-list-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './anime-list-item.component.html',
   styleUrl: './anime-list-item.component.scss'
 })
-export class AnimeListItemComponent {
+export class AnimeListItemComponent implements OnInit {
   @Input() item: any;
+  @Input() index!: number;
+  @Input() bigItem: boolean = false;
+
+  textBuilder = inject(TextBuilderService);
 
   constructor() {}
 
-  textBuilder = inject(TextBuilderService);
+  ngOnInit(): void {
+    
+  }
 }
