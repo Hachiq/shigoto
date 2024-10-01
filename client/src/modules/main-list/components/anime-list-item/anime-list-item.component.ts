3e7ec38474a88275c6ae6c73b67e1db61460fcf7
Andriiko <lesiv433@gmail.com>
2024-10-01 16:43:38 +0300

    styling, add play button

--- client/src/modules/main-list/components/anime-list-item/anime-list-item.component.ts
+++ client/src/modules/main-list/components/anime-list-item/anime-list-item.component.ts
@@ -1,15 +1,19 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
 import { TextBuilderService } from '../../services/text-builder.service';
 import { CommonModule } from '@angular/common';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-anime-list-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './anime-list-item.component.html',
   styleUrl: './anime-list-item.component.scss'
 })
 export class AnimeListItemComponent implements OnInit {
+  play = faPlay
+
   @Input() item: any;
   @Input() index!: number;
   @Input() bigItem: boolean = false;
