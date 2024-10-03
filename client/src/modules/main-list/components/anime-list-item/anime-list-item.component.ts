d68e3d7d1129996184f3abd77577af8051d21cb5
Andriiko <lesiv433@gmail.com>
2024-10-03 20:53:51 +0300

    add popover

--- client/src/modules/main-list/components/anime-list-item/anime-list-item.component.ts
+++ client/src/modules/main-list/components/anime-list-item/anime-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, Input, OnInit, ViewChild } from '@angular/core';
 import { TextBuilderService } from '../../services/text-builder.service';
 import { CommonModule } from '@angular/common';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
@@ -18,11 +18,50 @@ export class AnimeListItemComponent implements OnInit {
   @Input() index!: number;
   @Input() bigItem: boolean = false;
 
+  @ViewChild('poster', { static: true }) poster!: ElementRef;
+
+  popoverVisible = false;
+  popoverTop: number = 0;
+  popoverLeft: number = 0;
+
   textBuilder = inject(TextBuilderService);
 
   constructor() {}
 
   ngOnInit(): void {
     
+  }
+
+  showPopover(){
+    this.popoverVisible = true;
+    this.setPopoverPositioning();
+  }
+
+  hidePopover() {
+    this.popoverVisible = false;
+  }
+
+  setPopoverPositioning() {
+    const cardRect = this.poster.nativeElement.getBoundingClientRect();
+    const cardCenterX = cardRect.left + cardRect.width / 2;
+    const cardCenterY = cardRect.top + cardRect.height / 2 + window.scrollY;
+
+    // Set initial popover position
+    this.popoverLeft = cardCenterX;
+    this.popoverTop = cardCenterY;
+
+    // Adjust for viewport boundaries
+    const popoverElement = this.poster.nativeElement.querySelector('.popover');
+    const popoverRect = popoverElement?.getBoundingClientRect();
+
+    // Adjust left
+    if (this.popoverLeft + popoverRect?.width > window.innerWidth) {
+      this.popoverLeft = window.innerWidth - popoverRect.width - 10;
+    }
+
+    // Adjust top
+    if (this.popoverTop + popoverRect?.height > window.innerHeight) {
+      this.popoverTop = cardRect.bottom - popoverRect.height;
+    }
   }
 }
