8def44a56ccec3772813544b1d3ae69ca095c7e9
Andriiko <lesiv433@gmail.com>
2024-10-04 19:56:03 +0300

    fix popover positioning

--- client/src/modules/main-list/components/anime-list-item/anime-list-item.component.ts
+++ client/src/modules/main-list/components/anime-list-item/anime-list-item.component.ts
@@ -19,6 +19,7 @@ export class AnimeListItemComponent implements OnInit {
   @Input() bigItem: boolean = false;
 
   @ViewChild('poster', { static: true }) poster!: ElementRef;
+  @ViewChild('popover', { static: true }) popover!: ElementRef;
 
   popoverVisible = false;
   popoverTop: number = 0;
@@ -45,30 +46,27 @@ export class AnimeListItemComponent implements OnInit {
     this.popoverVisible = false;
   }
 
-  // TODO: Improve positioning
   setPopoverPositioning() {
     const cardRect = this.poster.nativeElement.getBoundingClientRect();
-    const cardCenterX = cardRect.left + cardRect.width / 2;
-    const cardCenterY = cardRect.top + cardRect.height / 2 + window.scrollY;
+    const popoverRect = this.popover?.nativeElement.getBoundingClientRect();
+
+    const cardCenterX = cardRect.left + cardRect.width / 2 + window.scrollX;
+    const cardCenterY = cardRect.top + cardRect.height / 2 + window.scrollY - popoverRect.height;
 
     // Set initial popover position
     this.popoverLeft = cardCenterX;
     this.popoverTop = cardCenterY;
 
     // Adjust for viewport boundaries
-    const popoverElement = this.poster.nativeElement.querySelector('.popover');
-    const popoverRect = popoverElement?.getBoundingClientRect();
 
     // Adjust left
     if (this.popoverLeft + popoverRect?.width > window.innerWidth) {
-      this.popoverLeft = window.innerWidth - popoverRect.width - 10;
+      this.popoverLeft-= popoverRect.width;
     }
 
     // Adjust top
-    if (this.popoverTop + popoverRect?.height > window.innerHeight) {
-      this.popoverTop = cardRect.bottom - popoverRect.height;
+    if (this.popoverTop < window.scrollY) {
+      this.popoverTop+= popoverRect.height;
     }
-
-    console.log("Calculated")
   }
 }
