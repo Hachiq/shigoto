16bf886824568752d2fb4cad3ce31881a0a53810
Andriiko <lesiv433@gmail.com>
2024-10-13 09:32:47 +0300

    dont add credentials for jikan requests

--- client/src/core/interceptors/with-credentials.interceptor.ts
+++ client/src/core/interceptors/with-credentials.interceptor.ts
@@ -1,9 +1,14 @@
 import { HttpHandlerFn, HttpRequest } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 export function withCredentialsInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
-  req = req.clone({
-    withCredentials: true
-  });
+  const shouldAddCredentials = req.url.includes(environment.apiUrl);
+
+  if (shouldAddCredentials) {
+    req = req.clone({
+      withCredentials: true
+    });
+  }
 
   return next(req);
 }
