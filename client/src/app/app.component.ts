fcadde9e27f5a1d594ef278145071dcd256dae7d
Andriiko <lesiv433@gmail.com>
2024-11-11 15:03:01 +0200

    remove unneccessary imports

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../core/components/header/header.component';
-import { SidebarComponent } from "../core/components/sidebar/sidebar.component";
 import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
