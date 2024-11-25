e31442d6468b3ec93572c384d3cf26a9d3fbcc37
Andriiko <lesiv433@gmail.com>
2024-11-25 15:39:27 +0200

    change default video

--- client/src/modules/common-shared/constants/video.ts
+++ client/src/modules/common-shared/constants/video.ts
@@ -1,5 +1,5 @@
 import { environment } from "../../../environments/environment";
 
 export const VIDEO = {
-  default: `${environment.apiUrl}/videos/303f4c5b-29b6-4612-bc72-b53051f863d6.mp4`
+  default: `${environment.apiUrl}/videos/13932500-ba87-45a2-afdb-31fec7b0abd3.mp4`
 }
